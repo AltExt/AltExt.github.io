7b5f1ffa13090d5a9555cc6f6edaee1db38ef5f8
AltExt <52500808+AltExt@users.noreply.github.com>
2024-09-26 00:22:26 +0100

    work began on tank tool

---
+++ js/calcTool.js
@@ -0,0 +1,10 @@
+
+function DefaultRadioClicked() 
+{
+	document.getElementById("conePitch_customValue").disabled = true;
+}
+
+function OtherRadioClicked()
+{
+	document.getElementById("conePitch_customValue").disabled = false;
+}
